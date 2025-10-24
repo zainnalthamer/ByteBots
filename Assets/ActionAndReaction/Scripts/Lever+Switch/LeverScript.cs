2118b2703097e01485bd68145e2f0e2b61c2e397
zainnalthamer <zainnalthamer@gmail.com>
2025-10-24 13:44:16 +0300

    carnival scene

---
+++ Assets/ActionAndReaction/Scripts/Lever+Switch/LeverScript.cs
@@ -0,0 +1,148 @@
+﻿
+/*
+    Made by Quint van Oorschot (Monqo Games)
+
+    Thank you for using the 'Action and reaction pack'!
+    You can use is for both commercial and personal projects.
+
+    Read the 'READ ME!.txt' to learn how to use this package
+
+    Do you need support? E-mail: monqogames@gmail.com
+    */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class LeverScript : MonoBehaviour {
+
+    [Header("Play function when the lever used:")]
+    public UnityEvent PlayWhenLeverUsed;
+    [Header("Play function when 'leverstate' equals true:")]
+    public UnityEvent WhenLeverStateEqualsTrue;
+    [Header("Play function when 'leverstate' equals false:")]
+    public UnityEvent WhenLeverStateEqualsFalse;
+    [Header("Play function when the cursor hovers over the lever:")]
+    public UnityEvent WhenHoversOverLever;
+    [Header("Important Variables:")]
+    public bool LeverState = false; //True = on  || False = off
+    [Space(5)]
+    [Header("Important Variable:")]
+    public bool UseAnimations;
+    public bool UseSoundEffects;
+    [Header("Animamtor settings:")]
+    public GameObject StickGameObject; //The object which moves because of the animation
+    [Header("Debug:")]
+    public bool PrintLeverState;
+    [Space(5)]
+    public Transform ToDrawLineTo;
+
+    //----------------------------------------------------------------------------------------------------------------------------------------
+
+    bool AnimationPlaying = false; //false when game starts
+    float AnimationCounter; //Resets script to default state when the animation is finished
+    AudioSource SoundEffect; //The sound effect when the lever is used
+    Animator anim;
+
+
+    private void Start() //When script starts for the first time
+    {
+        anim = StickGameObject.GetComponent<Animator>();
+        SoundEffect = this.GetComponent<AudioSource>();
+    }
+
+
+    private void OnMouseOver() //If cursor moves over a collider
+    {
+
+        WhenHoversOverLever.Invoke();
+
+        //0 = Left mousebutton || 1 = Right mousebutton || 2 = Middle mousebutton ||
+        if (Input.GetMouseButtonDown(0)) //If player presses mouse button
+        {
+            if (AnimationPlaying == false) //If nothing is moving already
+            {
+                PlayWhenLeverUsed.Invoke();
+
+                if (LeverState == false)
+                {
+                    LeverState = true;
+
+                    if (UseSoundEffects == true) //Only play sound effects if the creator wants to
+                    {
+                        SoundEffect.Play();
+                    }
+
+                    if (UseAnimations == true) //Only use animations if the creator wants to
+                    {
+                        anim.Play("LeverOnAnimation");
+                        AnimationPlaying = true;
+                    }
+
+                }
+                else
+                {
+                    LeverState = false;
+
+                    if (UseSoundEffects == true)
+                    {
+                        SoundEffect.Play();
+                    }
+
+                    if (UseAnimations == true) //Only use animations if the creator wants to
+                    {
+                        anim.Play("LeverOffAnimation");
+                        AnimationPlaying = true;
+                    }
+
+                }
+
+            }
+
+        }
+    }
+
+    private void Update()
+    {
+
+        if(ToDrawLineTo != null)
+        {
+
+            Debug.DrawLine(gameObject.transform.position, ToDrawLineTo.position);
+        }
+
+        if(PrintLeverState == true)
+        {
+            Debug.Log(LeverState);
+
+            if(ToDrawLineTo == null)
+            {
+                Debug.LogError("Where to draw to line to has not been assigned!");
+            }
+
+        }
+
+
+        if (LeverState == true)
+        {
+            WhenLeverStateEqualsTrue.Invoke();
+        }
+        else
+        {
+            WhenLeverStateEqualsFalse.Invoke();
+        }
+
+        if (AnimationPlaying == true)
+        {
+            AnimationCounter += 1 * Time.deltaTime;
+        }
+
+        if (AnimationCounter > 1.5f)
+        {
+            AnimationCounter = 0;
+            AnimationPlaying = false;
+        }
+
+    }
+}
---
+++ Assets/ActionAndReaction/Scripts/PressurePlate/PressurePlateScript.cs
@@ -0,0 +1,113 @@
+﻿
+/*
+    Made by Quint van Oorschot (Monqo Games)
+
+    Thank you for using the 'Ultimate interaction pack'!
+    You can use is for both commercial and personal projects.
+
+    I hope this helps you with your next game!
+
+    Read the 'READ ME!.txt' to learn how to use this package
+
+    Regards, Quint van Oorschot
+
+    */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class PressurePlateScript : MonoBehaviour {
+    
+
+    [Header("Enter the tag of the object that can trigger the plate:")]
+    public string CollisionTag; //Enter the tag of the object of collision
+    [Header("Play function once when 'CollisionTag' touches the pressure plate:")]
+    public UnityEvent WhenCollisionEnters; //The assigned functions will run once when the collision started
+    [Header("Play function every frame as long as the object touches the plate:")]
+    public UnityEvent WhenCollisionStay; //The assigned functions will run every frame until the collision stops
+    [Header("Play function once when the objects stops touching the plate:")]
+    public UnityEvent WhenCollisionExits; //The assigned functions will run when the collision ends
+    [Header("Settings:")]
+    public bool UseSoundEffects = true; //Do you want to use sound effects?
+    public bool UseAnimations = true; //Do you want to use animations?
+    [Header("Debug:")]
+    public bool PrintOnCollision = false; //Print a message in the console which prints the value of 'PrintMessage'
+    public string PrintMessage = "Collision detected!"; //Which message should be printed? (If 'PrintOnCollision' is enabled)
+
+    Animator anim;
+    AudioSource Sfx;
+
+
+    void Start () {
+
+        Sfx = this.GetComponent<AudioSource>(); //Assign the value of 'Sfx' when the script starts
+        anim = this.GetComponent<Animator>(); //Assign the value of 'anim' when the script starts
+
+        if (CollisionTag == "") //Send a message when no value of 'CollisioinTag' has been assigned
+        {
+            Debug.LogError("The value of 'CollisionTag' has not been assigned!"); //Prints the message
+        }
+
+    }
+
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        if(PrintOnCollision == true) //If the value of 'PrintOnCollision' equals True
+        {
+            Debug.Log(PrintMessage); //Prints the message
+        }
+
+        if(collision.gameObject.tag == CollisionTag) //If the collision object has a tag which equals the value of 'CollisionTag'
+        {
+
+            if (UseSoundEffects == true) //If the value of 'UseSoundEffects' equals true
+            {
+                Sfx.Play(); //Plays the sound effect of the pressure plate
+            }
+
+            if (UseAnimations == true) //If the value of 'UseAnimations' equals true
+            {
+                anim.Play("PressurePlateEnterAnimation"); //Plays the animation of pressure plate
+            }
+            WhenCollisionEnters.Invoke(); //Runs all assigned functions once when the collision starts
+        }
+
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        
+        if(collision.gameObject.tag == CollisionTag) //If the collision object has a tag which equals the value of 'CollisionTag'
+        {
+            WhenCollisionStay.Invoke(); //Runs all assigned functions every frame until the collision ends
+        }
+
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        
+        if(collision.gameObject.tag == CollisionTag) //If the collision object has a tag which equals the value of 'CollisionTag'
+        {
+
+            if (UseSoundEffects == true) //If the value of 'UseSoundEffects' equals true
+            {
+                Sfx.Play(); //Plays the sound effect of the pressure plate
+            }
+
+            if (UseAnimations == true) //If the value of 'UseAnimations' equals true  
+            {
+                anim.Play("PressurePlateReleaseAnimation"); //Plays the animation of pressure plate
+            }
+            WhenCollisionExits.Invoke(); //Runs all assigned functions once when the collision ends
+        }
+
+    }
+
+
+}
