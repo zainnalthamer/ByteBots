28781f7c2309af173d969068e62697197e33d2b4
zainnalthamer <zainnalthamer@gmail.com>
2025-10-27 23:07:57 +0300

    added resolution and display mode options

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
 using TMPro;
+using System.Collections.Generic;
 
 public class OptionsMenu : MonoBehaviour
 {
@@ -12,14 +13,50 @@ public class OptionsMenu : MonoBehaviour
 
     [Header("Graphics")]
     public TMP_Dropdown graphicsDropdown;
+    public TMP_Dropdown resolutionDropdown;
+    public TMP_Dropdown modeDropdown;
+
+    private Resolution[] resolutions;
+    private FullScreenMode currentMode;
 
     void Start()
     {
         graphicsDropdown.ClearOptions();
-        var options = new System.Collections.Generic.List<string>(QualitySettings.names);
-        graphicsDropdown.AddOptions(options);
+        graphicsDropdown.AddOptions(new List<string>(QualitySettings.names));
         graphicsDropdown.value = QualitySettings.GetQualityLevel();
         graphicsDropdown.RefreshShownValue();
+
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height + " @ " + resolutions[i].refreshRate + "Hz";
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = PlayerPrefs.GetInt("ResolutionIndex", currentResolutionIndex);
+        resolutionDropdown.RefreshShownValue();
+
+        modeDropdown.ClearOptions();
+        List<string> modes = new List<string>() { "Fullscreen", "Windowed", "Borderless" };
+        modeDropdown.AddOptions(modes);
+        currentMode = Screen.fullScreenMode;
+        modeDropdown.value = PlayerPrefs.GetInt("DisplayMode", ModeToIndex(currentMode));
+        modeDropdown.RefreshShownValue();
+
+        ApplyResolution();
+        ApplyDisplayMode();
 
         if (masterVolumeSlider != null)
             masterVolumeSlider.value = PlayerPrefs.GetFloat("MasterVol", 1f);
@@ -45,10 +82,55 @@ public class OptionsMenu : MonoBehaviour
         PlayerPrefs.SetFloat("SFXVol", value);
     }
 
-
     public void SetGraphicsQuality(int index)
     {
         QualitySettings.SetQualityLevel(index);
+    }
+
+    public void SetResolution(int index)
+    {
+        PlayerPrefs.SetInt("ResolutionIndex", index);
+        ApplyResolution();
+    }
+
+    private void ApplyResolution()
+    {
+        Resolution resolution = resolutions[PlayerPrefs.GetInt("ResolutionIndex", resolutions.Length - 1)];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreenMode);
+    }
+
+    public void SetDisplayMode(int index)
+    {
+        PlayerPrefs.SetInt("DisplayMode", index);
+        ApplyDisplayMode();
+    }
+
+    private void ApplyDisplayMode()
+    {
+        FullScreenMode mode = IndexToMode(PlayerPrefs.GetInt("DisplayMode", 0));
+        Screen.fullScreenMode = mode;
+    }
+
+    private int ModeToIndex(FullScreenMode mode)
+    {
+        switch (mode)
+        {
+            case FullScreenMode.FullScreenWindow: return 0;
+            case FullScreenMode.Windowed: return 1;
+            case FullScreenMode.ExclusiveFullScreen: return 2;
+            default: return 0;
+        }
+    }
+
+    private FullScreenMode IndexToMode(int index)
+    {
+        switch (index)
+        {
+            case 0: return FullScreenMode.FullScreenWindow;
+            case 1: return FullScreenMode.Windowed;
+            case 2: return FullScreenMode.ExclusiveFullScreen;
+            default: return FullScreenMode.FullScreenWindow;
+        }
     }
 
     public void BackToMenu(GameObject optionsPanel)
