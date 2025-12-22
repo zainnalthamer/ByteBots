c2245177559d102ad47869cd970a26f92e4a7844
Zainab Abdulhusain <202201685@student.polytechnic.bh>
2025-12-22 14:01:28 +0300

    H added cutscene

---
+++ Assets/StoryManager.cs
@@ -0,0 +1,36 @@
+using Fungus;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; 
+
+public class StoryManager : MonoBehaviour
+{
+
+    public GameObject cutsceneCameras;
+    public bool firstGame = true; 
+    public Flowchart introFlowchart;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if(ES3.KeyExists("firstGame"))
+        {
+            firstGame = false;
+            cutsceneCameras.SetActive(false);
+        }
+        else
+        {
+            cutsceneCameras.SetActive(true);
+            ES3.Save<bool>("firstGame", true);
+            SetupIntroScene();
+        }
+    }
+     
+
+
+    void SetupIntroScene()
+    {
+        introFlowchart.ExecuteBlock("IntroCutscene");
+    }
+}
